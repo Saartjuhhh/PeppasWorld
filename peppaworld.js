0012c911f8e831d7a5e1246b49b586289fe162e6
Saartjuhhh <72555381+Saartjuhhh@users.noreply.github.com>
2023-02-15 08:58:28 +0100

    Add files via upload

---
+++ peppaworld.js
@@ -0,0 +1,17 @@
+function hrefFunction(){  function hrefFunction(){
+    windows.location.href = "https://www.example.com";
+}
+
+//Function demonstrating the Replace Method
+
+function replaceFunction(){
+    windows.location.replace("https://www.example.com");
+}
+    windows.location.href = "https://www.example.com";
+}
+
+//Function demonstrating the Replace Method
+
+function replaceFunction(){
+    windows.location.replace("https://www.example.com");
+}
